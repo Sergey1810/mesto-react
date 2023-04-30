853a638bd60b9b7f139aa8937532c2accdca54c8
Sergey Lisovcev <deadlook15@yandex.ru>
2023-04-30 17:06:50 +0300

    feat: rev1

--- src/components/EditAvatarPopup.js
+++ src/components/EditAvatarPopup.js
@@ -18,7 +18,14 @@ export default function EditAvatarPopup(props) {
   return (
     <PopupWithForm isOpen={props.isOpen} onClose={props.onClose} name = 'avatar' title = 'Обновить аватар' onSubmit={handleSubmit}> 
       <div className="popup__field">
-        <input ref = {avatarRef} type="url" className="popup__input popup__input_type_urls" name="urls" placeholder="Ссылка на картинку" required/>
+        <input 
+          ref = {avatarRef} 
+          type="url" 
+          className="popup__input popup__input_type_urls" 
+          name="urls" 
+          placeholder="Ссылка на картинку" 
+          defaultValue={avatarRef || ''}
+          required/>
         <span className="popup__error-message urls-input-error"></span>
       </div>
     </PopupWithForm> 
